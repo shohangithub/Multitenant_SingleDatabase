0be7dad9892f4f244bec7c4e86382eaff0182adc
Md.Rafiul Islam Shohan <redlime.shohan@gmail.com>
2023-05-28 11:06:54 +0600

    create invoice for tenent

---
+++ Multitenant_SingleDatabase.InvoiceCode/Dtos/InvoiceSummaryDto.cs
@@ -0,0 +1,29 @@
+﻿using Multitenant_SingleDatabase.InvoiceCode.EfCoreClasses;
+
+namespace Multitenant_SingleDatabase.InvoiceCode.Dtos
+{
+    public class InvoiceSummaryDto
+    {
+        public int InvoiceId { get; set; }
+
+        public string InvoiceName { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public int NumItems { get; set; }
+
+        public double? TotalCost { get; set; }
+
+        public static IQueryable<InvoiceSummaryDto> SelectInvoices(IQueryable<Invoice> invoices)
+        {
+            return invoices.Select(x => new InvoiceSummaryDto
+            {
+                InvoiceId = x.InvoiceId,
+                InvoiceName = x.InvoiceName,
+                DateCreated = x.DateCreated,
+                NumItems = x.LineItems.Count,
+                TotalCost = x.LineItems.Select(y => (double?)y.TotalPrice).Sum()
+            });
+        }
+    }
+}
---
+++ Multitenant_SingleDatabase/Controllers/InvoiceController.cs
@@ -0,0 +1,55 @@
+﻿using AuthPermissions.AspNetCore;
+using AuthPermissions.BaseCode.PermissionsCode;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Multitenant_SingleDatabase.InvoiceCode.AppStart;
+using Multitenant_SingleDatabase.InvoiceCode.Dtos;
+using Multitenant_SingleDatabase.InvoiceCode.EfCoreClasses;
+using Multitenant_SingleDatabase.InvoiceCode.EfCoreCode;
+using Multitenant_SingleDatabase.InvoiceCode.Services;
+using Multitenant_SingleDatabase.PermissionsCode;
+
+namespace Multitenant_SingleDatabase.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private readonly InvoicesDbContext _context;
+
+        public InvoiceController(InvoicesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(string message)
+        {
+            ViewBag.Message = message;
+
+            var listInvoices = User.HasPermission(SingleDbPermissions.InvoiceRead)
+                ? await InvoiceSummaryDto.SelectInvoices(_context.Invoices)
+                    .OrderByDescending(x => x.DateCreated)
+                    .ToListAsync()
+                : null;
+            return View(listInvoices);
+        }
+
+        [HasPermission(SingleDbPermissions.InvoiceCreate)]
+        public IActionResult CreateInvoice()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [HasPermission(SingleDbPermissions.InvoiceCreate)]
+        public async Task<IActionResult> CreateInvoice(Invoice invoice)
+        {
+            var builder = new ExampleInvoiceBuilder(null);
+            var newInvoice = builder.CreateRandomInvoice(AddTenantNameClaim.GetTenantNameFromUser(User), invoice.InvoiceName);
+            _context.Add(newInvoice);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", new { message = $"Added the invoice '{newInvoice.InvoiceName}'." });
+        }
+
+    }
+}
