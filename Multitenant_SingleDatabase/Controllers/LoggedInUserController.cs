2aba72bf2917e02d3e48307effe82c38d5901bc4
Md.Rafiul Islam Shohan <redlime.shohan@gmail.com>
2023-05-28 11:16:15 +0600

    add login user inforamtion

---
+++ Multitenant_SingleDatabase/Controllers/LoggedInUserController.cs
@@ -0,0 +1,37 @@
+﻿using AuthPermissions.AdminCode;
+using AuthPermissions.BaseCode.CommonCode;
+using AuthPermissions.BaseCode.PermissionsCode;
+using Common.CommonAdmin;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Multitenant_SingleDatabase.Controllers
+{
+    public class LoggedInUserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(User);
+        }
+
+        public async Task<IActionResult> AuthUserInfo([FromServices] IAuthUsersAdminService service)
+        {
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                var userId = User.GetUserIdFromUser();
+                var status = await service.FindAuthUserByUserIdAsync(userId);
+
+                if (status.HasErrors)
+                    return RedirectToAction("ErrorDisplay", "AuthUsers",
+                        new { errorMessage = status.GetAllErrors() });
+
+                return View(AuthUserDisplay.DisplayUserInfo(status.Result));
+            }
+            return View((AuthUserDisplay)null);
+        }
+
+        public IActionResult UserPermissions([FromServices] IUsersPermissionsService service)
+        {
+            return View(service.PermissionsFromUser(HttpContext.User));
+        }
+    }
+}
